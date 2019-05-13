c0075528c0b17fd24d0fda6c28334fef8849d76d
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-05-13 14:07:11 -0500

    Java Syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,43 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.print("Hello World");
-    }
+    // "Hello World" Java edition
+    public static void main(String[] args) {
+        {
+            System.out.println("Hello World");
+        }
+        {
+            int myFavoriteNumber = 32;
+            System.out.println(myFavoriteNumber);
+        }
+        {
+            String myString = "ok den";
+            System.out.println(myString);
+        }
+        {
+            float myNumber = 3.14F;
+            System.out.println(myNumber);
+        }
+        {
+            int x = 5;
+            System.out.println(++x);
+            System.out.println(x);
+        }
+        {
+            int x = 4;
+            int nine = x + 5;
+            System.out.println(nine);
+        }
+        {
+            int x = 3;
+            int y = 4;
+            int twelve = x * y;
+            System.out.println(twelve);
+        }
+        {
+            int x = 10;
+            int y = 2;
+            int five = x / y;
+            int negEight = y - x;
+            System.out.println(five);
+        }
+        }
 }
