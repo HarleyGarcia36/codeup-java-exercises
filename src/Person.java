db69cc4f4dca9bcd1d986846986d16b929f10917
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-05-17 11:56:05 -0500

    wow

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+// Exercise 1 Object basics
 public class Person {
 
     private String name;
@@ -23,9 +24,16 @@ public class Person {
         harley.sayHello();
     }
 
-    { Person person1 = new Person("John");
-    Person person2 = new Person("John");
-System.out.println(person1.getName().equals(person2.getName()));
-System.out.println(person1 == person2); }
+
+// Exercise 2 understanding references
+//    public static void main(String[] args) {
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//    }
 
 }
